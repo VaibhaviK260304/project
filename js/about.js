9ce47ca4f7ead9c73710bf2b87446685e8af5b30
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-05-16 11:02:28 +0530

    added home page and about page info

---
+++ js/about.js
@@ -0,0 +1,24 @@
+  function about(){
+    document.getElementById("home").style.color="white";
+    document.getElementById("about").style.color="";
+    document.getElementById("contact").style.color="white";
+    document.getElementById("time").style.color="White";
+  }
+  function contact(){
+    document.getElementById("home").style.color="white";
+    document.getElementById("about").style.color="white";
+    document.getElementById("contact").style.color="";
+    document.getElementById("time").style.color="White";  
+  }
+  function home(){
+    document.getElementById("home").style.color="";
+    document.getElementById("about").style.color="white";
+    document.getElementById("contact").style.color="white";
+    document.getElementById("time").style.color="white";
+  }
+  function time(){
+    document.getElementById("home").style.color="white";
+    document.getElementById("about").style.color="white";
+    document.getElementById("contact").style.color="white";
+    document.getElementById("time").style.color="";
+  }
