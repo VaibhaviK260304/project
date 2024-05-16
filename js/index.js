40f94843abd6c7014d230e924c2ed1c73dffccae
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-05-17 00:19:12 +0530

    path changed

--- js/index.js
+++ js/index.js
@@ -2,14 +2,26 @@ function about(){
     document.getElementById("about").style.color="";
     document.getElementById("contact").style.color="white";
     document.getElementById("time").style.color="White";
+    document.getElementById("home").style.color="white";
+
   }
   function contact(){
     document.getElementById("about").style.color="white";
     document.getElementById("contact").style.color="";
-    document.getElementById("time").style.color="White";  
+    document.getElementById("time").style.color="White";
+    document.getElementById("home").style.color="white";
+  
   }
   function time(){
     document.getElementById("about").style.color="white";
     document.getElementById("contact").style.color="white";
     document.getElementById("time").style.color="";
+    document.getElementById("home").style.color="white";
+
+  }
+  function home(){
+    document.getElementById("about").style.color="white";
+    document.getElementById("contact").style.color="white";
+    document.getElementById("time").style.color="white";
+    document.getElementById("home").style.color="";
   }
