cccf578aa15e0cb23e105513ecc28ca8515d5f0f
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-05-17 00:05:04 +0530

    Js added for new page

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 function about(){
     document.getElementById("about").style.color="";
     document.getElementById("contact").style.color="white";
-    document.getElementById("feedback").style.color="White";
+    document.getElementById("time").style.color="White";
   }
   function contact(){
     document.getElementById("about").style.color="white";
     document.getElementById("contact").style.color="";
-    document.getElementById("feedback").style.color="White";  
+    document.getElementById("time").style.color="White";  
   }
-  function feedback(){
+  function time(){
     document.getElementById("about").style.color="white";
     document.getElementById("contact").style.color="white";
-    document.getElementById("feedback").style.color="";
+    document.getElementById("time").style.color="";
   }
---
+++ js/kurti.js
@@ -0,0 +1,304 @@
+function inc()
+{
+    const quantitytext  = document.getElementById("qty");
+    const showquantity = document.getElementById("quantity-text");
+
+    const  totaltext = document.getElementById("total-text") ;
+
+    let quantity = parseInt(quantitytext.innerText);
+
+    if(quantity<10)
+    {
+        quantitytext.innerText = ++quantity;
+    }
+    else{
+        alert("Maximum Quantity Reached!");
+    }
+    showquantity.innerText=`Quantity : ${quantity} x 599`;
+    totaltext.innerText= `Total :  ${quantity*599}`;
+}
+function dec()
+{
+    const quantitytext  = document.getElementById("qty");
+    const showquantity = document.getElementById("quantity-text");
+    const  totaltext = document.getElementById("total-text") ;
+
+    let quantity = parseInt(quantitytext.innerText);
+    
+    if(quantity > 1)
+    {
+    quantitytext.innerText = --quantity;
+    }
+    else
+    {
+        alert('Quantity cannot be less than 1');
+    }
+    showquantity.innerText=`Quantity : ${quantity} x 599`;
+    totaltext.innerText= `Total :  ${quantity*599}`;
+}
+function inc1()
+{
+    const quantitytext1  = document.getElementById("qty1");
+    const showquantity1 = document.getElementById("quantity-text1");
+
+    const  totaltext1 = document.getElementById("total-text1") ;
+
+    let quantity1 = parseInt(quantitytext1.innerText);
+
+    if(quantity1<10)
+    {
+        quantitytext1.innerText = ++quantity1;
+    }
+    else{
+        alert("Maximum Quantity Reached!");
+    }
+    showquantity1.innerText=`Quantity : ${quantity1} x 599`;
+    totaltext1.innerText= `Total :  ${quantity1*599}`;
+}
+function dec1()
+{
+    const quantitytext1  = document.getElementById("qty1");
+    const showquantity1 = document.getElementById("quantity-text1");
+    const  totaltext1 = document.getElementById("total-text1") ;
+
+    let quantity1 = parseInt(quantitytext1.innerText);
+    
+    if(quantity1 > 1)
+    {
+    quantitytext1.innerText = --quantity1;
+    }
+    else
+    {
+        alert('Quantity cannot be less than 1');
+    }
+    showquantity1.innerText=`Quantity : ${quantity1} x 599`;
+    totaltext1.innerText= `Total :  ${quantity1*599}`;
+}
+function inc2()
+{
+    const quantitytext2  = document.getElementById("qty2");
+    const showquantity2 = document.getElementById("quantity-text2");
+
+    const  totaltext2 = document.getElementById("total-text2") ;
+
+    let quantity2 = parseInt(quantitytext2.innerText);
+
+    if(quantity2<10)
+    {
+        quantitytext2.innerText = ++quantity2;
+    }
+    else{
+        alert("Maximum Quantity Reached!");
+    }
+    showquantity2.innerText=`Quantity : ${quantity2} x 599`;
+    totaltext2.innerText= `Total :  ${quantity2*599}`;
+}
+function dec2()
+{
+    const quantitytext2  = document.getElementById("qty2");
+    const showquantity2 = document.getElementById("quantity-text2");
+    const  totaltext2 = document.getElementById("total-text2") ;
+
+    let quantity2 = parseInt(quantitytext2.innerText);
+    
+    if(quantity2 > 1)
+    {
+    quantitytext2.innerText = --quantity2;
+    }
+    else
+    {
+        alert('Quantity cannot be less than 1');
+    }
+    showquantity2.innerText=`Quantity : ${quantity2} x 599`;
+    totaltext2.innerText= `Total :  ${quantity2*599}`;
+}
+function inc3()
+{
+    const quantitytext3  = document.getElementById("qty3");
+    const showquantity3 = document.getElementById("quantity-text3");
+
+    const  totaltext3 = document.getElementById("total-text3") ;
+
+    let quantity3 = parseInt(quantitytext3.innerText);
+
+    if(quantity3<10)
+    {
+        quantitytext3.innerText = ++quantity3;
+    }
+    else{
+        alert("Maximum Quantity Reached!");
+    }
+    showquantity3.innerText=`Quantity : ${quantity3} x 599`;
+    totaltext3.innerText= `Total :  ${quantity3*599}`;
+}
+function dec3()
+{
+    const quantitytext3  = document.getElementById("qty3");
+    const showquantity3 = document.getElementById("quantity-text3");
+    const  totaltext3 = document.getElementById("total-text3") ;
+
+    let quantity3 = parseInt(quantitytext3.innerText);
+    
+    if(quantity3 > 1)
+    {
+    quantitytext3.innerText = --quantity3;
+    }
+    else
+    {
+        alert('Quantity cannot be less than 1');
+    }
+    showquantity3.innerText=`Quantity : ${quantity3} x 599`;
+    totaltext3.innerText= `Total :  ${quantity3*599}`;
+}
+function inc4()
+{
+    const quantitytext4  = document.getElementById("qty4");
+    const showquantity4 = document.getElementById("quantity-text4");
+
+    const  totaltext4 = document.getElementById("total-text4") ;
+
+    let quantity4 = parseInt(quantitytext4.innerText);
+
+    if(quantity4<10)
+    {
+        quantitytext4.innerText = ++quantity4;
+    }
+    else{
+        alert("Maximum Quantity Reached!");
+    }
+    showquantity4.innerText=`Quantity : ${quantity4} x 650`;
+    totaltext4.innerText= `Total :  ${quantity4*650}`;
+}
+function dec4()
+{
+    const quantitytext4  = document.getElementById("qty4");
+    const showquantity4 = document.getElementById("quantity-text4");
+    const  totaltext4 = document.getElementById("total-text4") ;
+
+    let quantity4 = parseInt(quantitytext4.innerText);
+    
+    if(quantity4 > 1)
+    {
+    quantitytext4.innerText = --quantity4;
+    }
+    else
+    {
+        alert('Quantity cannot be less than 1');
+    }
+    showquantity4.innerText=`Quantity : ${quantity4} x 650`;
+    totaltext4.innerText= `Total :  ${quantity4*650}`;
+}
+function inc5()
+{
+    const quantitytext5  = document.getElementById("qty5");
+    const showquantity5 = document.getElementById("quantity-text5");
+
+    const  totaltext5 = document.getElementById("total-text5") ;
+
+    let quantity5 = parseInt(quantitytext5.innerText);
+
+    if(quantity5<10)
+    {
+        quantitytext5.innerText = ++quantity5;
+    }
+    else{
+        alert("Maximum Quantity Reached!");
+    }
+    showquantity5.innerText=`Quantity : ${quantity5} x 650`;
+    totaltext5.innerText= `Total :  ${quantity5*650}`;
+}
+function dec5()
+{
+    const quantitytext5  = document.getElementById("qty5");
+    const showquantity5 = document.getElementById("quantity-text5");
+    const  totaltext5 = document.getElementById("total-text5") ;
+
+    let quantity5 = parseInt(quantitytext5.innerText);
+    
+    if(quantity5 > 1)
+    {
+    quantitytext5.innerText = --quantity5;
+    }
+    else
+    {
+        alert('Quantity cannot be less than 1');
+    }
+    showquantity5.innerText=`Quantity : ${quantity5} x 650`;
+    totaltext5.innerText= `Total :  ${quantity5*650}`;
+}
+function inc6()
+{
+    const quantitytext6 = document.getElementById("qty6");
+    const showquantity6 = document.getElementById("quantity-text6");
+
+    const  totaltext6 = document.getElementById("total-text6") ;
+
+    let quantity6 = parseInt(quantitytext6.innerText);
+
+    if(quantity6<10)
+    {
+        quantitytext6.innerText = ++quantity6;
+    }
+    else{
+        alert("Maximum Quantity Reached!");
+    }
+    showquantity6.innerText=`Quantity : ${quantity6} x 499`;
+    totaltext6.innerText= `Total :  ${quantity6*499}`;
+}
+function dec6()
+{
+    const quantitytext6  = document.getElementById("qty6");
+    const showquantity6 = document.getElementById("quantity-text6");
+    const  totaltext6 = document.getElementById("total-text6") ;
+
+    let quantity6 = parseInt(quantitytext6.innerText);
+    
+    if(quantity6 > 1)
+    {
+    quantitytext6.innerText = --quantity6;
+    }
+    else
+    {
+        alert('Quantity cannot be less than 1');
+    }
+    showquantity6.innerText=`Quantity : ${quantity6} x 499`;
+    totaltext6.innerText= `Total :  ${quantity6*499}`;
+}
+function inc7()
+{
+    const quantitytext7  = document.getElementById("qty7");
+    const showquantity7 = document.getElementById("quantity-text7");
+
+    const  totaltext7 = document.getElementById("total-text7") ;
+
+    let quantity7 = parseInt(quantitytext7.innerText);
+
+    if(quantity7<10)
+    {
+        quantitytext7.innerText = ++quantity7;
+    }
+    else{
+        alert("Maximum Quantity Reached!");
+    }
+    showquantity7.innerText=`Quantity : ${quantity7} x 699`;
+    totaltext7.innerText= `Total :  ${quantity7*699}`;
+}
+function dec7()
+{
+    const quantitytext7  = document.getElementById("qty7");
+    const showquantity7 = document.getElementById("quantity-text7");
+    const  totaltext7 = document.getElementById("total-text7") ;
+
+    let quantity7 = parseInt(quantitytext7.innerText);
+    
+    if(quantity7 > 1)
+    {
+    quantitytext7.innerText = --quantity7;
+    }
+    else
+    {
+        alert('Quantity cannot be less than 1');
+    }
+    showquantity7.innerText=`Quantity : ${quantity7} x 699`;
+    totaltext7.innerText= `Total :  ${quantity7*699}`;
+}
