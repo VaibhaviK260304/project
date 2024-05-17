a3b88213df6ee669f47ad5675b58c6d54f475559
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-05-17 16:08:22 +0530

    contact page

---
+++ js/contact.js
@@ -0,0 +1,10 @@
+function contactus(){
+    const input=document.getElementById("no");
+    const input1=document.getElementById("no1");
+    const input2=document.getElementById("no2");
+    alert("THANK YOU! We will contact you soon. \nWe'll provide information through mail.");
+
+    input.value=' ';
+    input1.value=' ';
+    input2.value=' ';
+}
