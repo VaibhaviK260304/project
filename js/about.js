7b83d4d8148e7c3a6d54e106b25080cb319cd89c
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-05-17 17:04:07 +0530

    contact page done

---
+++ js/about.js
@@ -0,0 +1,15 @@
+function contact(){
+    document.getElementById("contact").style.color="";
+    document.getElementById("time").style.color="White";
+    document.getElementById("home").style.color="white";
+  }
+  function time(){
+    document.getElementById("contact").style.color="white";
+    document.getElementById("time").style.color="";
+    document.getElementById("home").style.color="white";
+  }
+  function home(){
+    document.getElementById("contact").style.color="white";
+    document.getElementById("time").style.color="white";
+    document.getElementById("home").style.color="";
+  }
--- js/contact.js
+++ js/contact.js
@@ -2,9 +2,31 @@ function contactus(){
     const input=document.getElementById("no");
     const input1=document.getElementById("no1");
     const input2=document.getElementById("no2");
-    alert("THANK YOU! We will contact you soon. \nWe'll provide information through mail.");
+    if(input == " "||input1 == " "||input2 == " "){
+        alert("Fill the complete form!")
+    }
+    else{
+        alert("THANK YOU! We will contact you soon. \nWe'll provide information through mail.");
+    }
 
     input.value=' ';
     input1.value=' ';
     input2.value=' ';
 }
+function about(){
+    document.getElementById("about").style.color="";
+    document.getElementById("time").style.color="White";
+    document.getElementById("home").style.color="white";
+
+  }
+  function time(){
+    document.getElementById("about").style.color="white";
+    document.getElementById("time").style.color="";
+    document.getElementById("home").style.color="white";
+
+  }
+  function home(){
+    document.getElementById("about").style.color="white";
+    document.getElementById("time").style.color="white";
+    document.getElementById("home").style.color="";
+  }
